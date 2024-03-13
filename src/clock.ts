bc91c60671a94b3e849b1063570fd47f00686870
JinKyu <rlawlsrb167@naver.com>
2024-03-14 00:56:36 +0900

    Feat: 월 일 요일 기능 추가

--- src/clock.ts
+++ src/clock.ts
@@ -1,11 +1,16 @@
-const clock = document.querySelector("#clock")! as HTMLDivElement;
+const clock = document.querySelector("#clock")! as HTMLHeadingElement;
+const dayInfo = document.querySelector("#day-info")! as HTMLHeadingElement;
 
 function getCurrentTime() {
   const date = new Date();
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
-  const seconds = String(date.getSeconds()).padStart(2, "0");
-  clock.textContent = `${hours}:${minutes}:${seconds}`;
+  const month = String(date.getMonth() + 1);
+  const dateDay = String(date.getDate());
+  const days = ["일", "월", "화", "수", "목", "금", "토"];
+  const day = days[date.getDay()];
+  clock.textContent = `${hours}:${minutes}`;
+  dayInfo.textContent = `${month}월 ${dateDay}일 ${day}요일`;
 }
 
 getCurrentTime();
