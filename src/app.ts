98871ee6a8342068182484bc47b88e62974b495c
JinKyu <rlawlsrb167@naver.com>
2024-03-18 20:46:30 +0900

    Feat: app 보여주기 숨기기 기능

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,35 @@
 {
   const app = document.querySelector(".screen__center")! as HTMLDivElement;
+  const drawingBoard = document.querySelector(
+    ".app__drawing-board"
+  )! as HTMLDivElement;
+  const musicPlayer = document.querySelector(
+    ".app__music-player"
+  )! as HTMLDivElement;
+  const calculator = document.querySelector(
+    ".app_calculator"
+  )! as HTMLDivElement;
 
+  function toggleClassName(element: HTMLElement) {
+    if (element.className.includes("active")) {
+      element.classList.remove("active");
+    } else {
+      element.classList.add("active");
+    }
+  }
   app.addEventListener("click", (e: any) => {
     switch (e.target.className) {
+      case "fa-solid fa-palette":
+        toggleClassName(drawingBoard);
+        break;
+      case "fa-solid fa-music":
+        toggleClassName(musicPlayer);
+        break;
+      case "fa-solid fa-calculator":
+        toggleClassName(calculator);
+        break;
+      default:
+        break;
     }
   });
 }
